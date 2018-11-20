118935f77dc07f43ad3de62ecdf9727d9bcb6314
J. Busch <josobu33@gmail.com>
2018-11-20 22:34:26 +0100

    Smooth moving

--- Dribble/Assets/_Scripts/CharBehaviour.cs
+++ Dribble/Assets/_Scripts/CharBehaviour.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class CharBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private bool isMovingRight;
+    private bool isMovingLeft;
+
+    // Use this for initialization
+    void Start () {
+        isMovingRight = false;
+        isMovingLeft = false;
 	}
 	
 	// Update is called once per frame 
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.LeftArrow)){
-			Vector3 position = this.transform.position;
-			position.x-=0.5f;
-			this.transform.position = position;
-		}
-		
-		if(Input.GetKeyDown(KeyCode.RightArrow)){
-			Vector3 position = this.transform.position;
-			position.x+=0.5f;
-			this.transform.position = position;
-		}
+        if(isMovingLeft || isMovingRight){
+            transform.position += move();      
+        }else{
+            isMovingLeft = Input.GetKeyDown(KeyCode.LeftArrow);
+            isMovingRight = Input.GetKeyDown(KeyCode.RightArrow);
+        }
 	}
+    private Vector3 move(){
+        float step = 0;
+        if(isMovingLeft){
+            isMovingLeft = isKeyReleased(KeyCode.LeftArrow);
+            step = -10.0f;
+        }
+        if(isMovingRight){
+            isMovingRight = isKeyReleased(KeyCode.RightArrow);
+            step = 10.0f;
+        }
+        return new Vector3(step * Time.deltaTime, 0,0);
+    }
+
+    private bool isKeyReleased(KeyCode keyCode){
+        return !Input.GetKeyUp(keyCode);
+    }
 }
